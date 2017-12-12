467395695c34854110b17419f8b96d1c4d99c4fb
Yaroslav Volohov <yvolohov@gmail.com>
2017-12-12 18:51:29 +0200

    page loading

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import m from 'mithril';
+import PageLoading from './components/page-loading';
 import PageMenu from './components/page-menu';
 import PageFillInBulk from './components/page-fill-in-bulk';
 import PageGetDocumentFields from './components/page-get-document-fields';
 import PageGetFormFields from './components/page-get-form-fields';
 import PageGetEditorAccessLink from './components/page-get-editor-access-link';
 
-m.route(document.getElementById('content'), '/page-menu', {
+m.route(document.getElementById('content'), '/page-loading', {
+  '/page-loading': PageLoading,
   '/page-menu': PageMenu,
   '/page-fill-in-bulk': PageFillInBulk,
   '/page-get-document-fields': PageGetDocumentFields,
--- src/labels.js
+++ src/labels.js
@@ -4,5 +4,6 @@ export default {
   l_2: 'Extract data from filled documents to the sheet',
   l_3: 'Extract data from filled forms to the sheet',
   l_4: 'Get editor access links for documents',
-  l_5: 'Template document'
+  l_5: 'Template document',
+  l_6: 'Loading ...'
 };
