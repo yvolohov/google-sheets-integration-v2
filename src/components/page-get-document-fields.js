4bcd3fdb3c7366df7819faa6c057170d2c68fe03
Yaroslav Volohov <yvolohov@gmail.com>
2018-01-10 19:37:40 +0200

    improved page get document fields

--- src/components/page-get-document-fields.js
+++ src/components/page-get-document-fields.js
@@ -1,9 +1,29 @@
 import m from 'mithril';
+import PageLoading from './page-loading';
 import RowHeader from './common/row-header';
 import labels from '../labels';
+import modelsLoader from '../models/models-loader';
 
 class PageGetDocumentFields {
+  constructor() {
+    this.models = ['documents'];
+  }
+
+  oninit() {
+    let notLoaded = modelsLoader.getNotLoadedModels(this.models);
+
+    if (notLoaded.length > 0) {
+      modelsLoader.loadModels(notLoaded, () => {m.redraw();});
+    }
+  }
+
   view() {
+    let notLoaded = modelsLoader.getNotLoadedModels(this.models);
+
+    if (notLoaded.length > 0) {
+      return m(PageLoading);
+    }
+
     return m('div', {class: 'container'}, [
       m(RowHeader, {pageLabel: labels.l_2})
     ]);
